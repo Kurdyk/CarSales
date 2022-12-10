e371f50b8eb3c2973ea0ce60eefb4fe65dec2afb
kalousdi <leah.kalousdian@kalousdian.fr>
2022-12-10 15:30:18 +0100

    classe inutile suppr, ajout .pdf, see docs

--- src/main/java/PdfHandler/PdfDocument.java
+++ src/main/java/PdfHandler/PdfDocument.java
@@ -3,8 +3,21 @@ package PdfHandler;
 import Content.Order;
 import Content.Vehicles.Vehicle;
 
+/**
+ * The type Pdf document.
+ */
 public abstract class PdfDocument {
+    /**
+     * The Order.
+     */
     protected Order order;
+    /**
+     * The Path.
+     */
     protected String path;
+    /**
+     * The Title.
+     */
     protected String title;
 }
+
